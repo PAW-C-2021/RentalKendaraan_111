ca6083b3be6aeb61b7e358caeae4a7263335302f
PebriKurniaSandi <fksandi07@gmail.com>
2021-11-09 21:08:08 +0700

    Add project files.

---
+++ RentalKendaraan_111/Models/JenisKendaraan.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace RentalKendaraan_111.Models
+{
+    public partial class JenisKendaraan
+    {
+        public JenisKendaraan()
+        {
+            Kendaraans = new HashSet<Kendaraan>();
+        }
+
+        public int IdJenisKendaraan { get; set; }
+        public string NamaJenisKendaraan { get; set; }
+
+        public virtual ICollection<Kendaraan> Kendaraans { get; set; }
+    }
+}
